5e8ef3840d464682d014dddef2be9b5dd6dfdb25
Peter <lin.peter@outlook.fr>
2023-01-06 23:37:00 +0100

    changements

--- chatroom/models.py
+++ chatroom/models.py
@@ -5,3 +5,8 @@ class Room(models.Model):
     roomName=models.CharField(max_length=50)
     def __str__(self):
         return self.roomName
+
+class Message(models.Model):
+    message=models.CharField(max_length=10000)
+    def __str__(self):
+        return self.message
