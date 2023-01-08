90f825dff4bc8d8563063fdd0f1c9813222a4336
MatthiasZdravkovic <Matthias.Zdravkovic2@gmail.com>
2023-01-08 16:59:16 +0100

    Messages sauvegardés sur le chat + room.html

---
+++ chatroom/templates/chatroom/send.js
@@ -0,0 +1,13 @@
+
+
+function sendMessage() {
+  // Get the message text from the input field
+  var messageText = document.getElementById("message-input").value;
+  
+  // Clear the message input
+  document.getElementById("message-input").value = "";
+  
+  // Update the chat window with the new message
+  var chatWindow = document.getElementById("chat-window");
+  chatWindow.innerHTML += "<p>" + messageText + "</p>";
+}
