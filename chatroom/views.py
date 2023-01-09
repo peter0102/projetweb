3cb3e53df5efd3077ced45fb7a24520351273aa5
Peter <lin.peter@outlook.fr>
2023-01-09 21:02:31 +0100

    commentaires

--- chatroom/views.py
+++ chatroom/views.py
@@ -14,7 +14,7 @@ def home(request):
         else :
             newRoom=Room(roomName=roomNameWanted)
             newRoom.save()
-            return redirect('/chatroom/room_name='+roomNameWanted+'/')
+            return redirect('/chatroom/room_name='+roomNameWanted+'/') #crée des url personnalisées en fonction du nom du salon
     allRooms=Room.objects.all()
     return render(request,'chatroom/home.html',{'allRooms': allRooms})
 
@@ -22,21 +22,21 @@ def home(request):
 def rooms(request,roomName):
     nameOfTheRoom=roomName
     username=request.user.get_username()
-    roomId=Room.objects.get(roomName=nameOfTheRoom)
-    return render(request,'chatroom/room.html',{ 'nameOfTheRoom' : nameOfTheRoom, 'username':username, 'roomId':roomId})
+    chatRoomName=Room.objects.get(roomName=nameOfTheRoom)
+    return render(request,'chatroom/room.html',{ 'nameOfTheRoom' : nameOfTheRoom, 'username':username, 'chatRoomName':chatRoomName})
 
 def sendMessage(request):
     if request.method=='POST':
         messageSent=request.POST['messageSent']
-        roomId=request.POST['roomId']
+        chatRoomName=request.POST['chatRoomName']
         user=request.user.get_username()
-        newMessage=Message(message=messageSent,where=roomId,who=user) #on récupère le salon où a été envoyé le message
+        newMessage=Message(message=messageSent,where=chatRoomName,who=user) #on sauvegarde le message
         newMessage.save()
         return JsonResponse({'success': True})
 
 def getMessage(request,roomName):
     nameOfTheRoom=roomName
-    roomId=Room.objects.get(roomName=nameOfTheRoom)
-    allMessagesList=Message.objects.filter(where=roomId.id) #on ne récupère que les messages du salon souhaité
+    chatRoomName=Room.objects.get(roomName=nameOfTheRoom)
+    allMessagesList=Message.objects.filter(where=chatRoomName) #on ne récupère que les messages du salon souhaité
     allMessagesJson=[{'allMessagesValue':message.message, 'who':message.who} for message in allMessagesList] #on récupère la valeur allMessageValue message.message(champ de la classe Message), un seul message aurait donné la classe, le deuxième donne la ième valeur
     return JsonResponse({"allMessagesList":allMessagesJson})
